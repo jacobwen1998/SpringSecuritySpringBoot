0e47c6cb159a12e57267384a6c697c30995a0b0c
xuezh <jacobwen1998@gmail.com>
2026-06-20 01:19:45 +0800

    正在配置mybatis-plus，Windows的密码是991600

--- src/main/java/com/jacob/springsecurityspringboot/mapper/UsersMapper.java
+++ src/main/java/com/jacob/springsecurityspringboot/mapper/UsersMapper.java
@@ -1,4 +1,7 @@
 package com.jacob.springsecurityspringboot.mapper;
 
-public interface UsersMapper {
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jacob.springsecurityspringboot.entity.Users;
+
+public interface UsersMapper extends BaseMapper<Users> {
 }
--- src/main/java/com/jacob/springsecurityspringboot/service/MyUserDetailService.java
+++ src/main/java/com/jacob/springsecurityspringboot/service/MyUserDetailService.java
@@ -1,5 +1,10 @@
 package com.jacob.springsecurityspringboot.service;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.jacob.springsecurityspringboot.entity.Users;
+import com.jacob.springsecurityspringboot.mapper.UsersMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -7,11 +12,23 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class MyUserDetailService implements UserDetailsService {
+    @Autowired
+    private UsersMapper usersMapper;
     @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        String pwd = "dummmy";
-        return new User(username, pwd, AuthorityUtils.commaSeparatedStringToAuthorityList("admin,"));
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        QueryWrapper<Users> queryWrapper = new QueryWrapper<Users>();
+        queryWrapper.eq("username", s);
+        Users users = (Users) usersMapper.selectOne(queryWrapper);
+        if (users == null) {
+            throw new UsernameNotFoundException("username not found");
+        }
+        System.out.println("username : " + users.getUsername());
+        List<GrantedAuthority> auths = AuthorityUtils.commaSeparatedStringToAuthorityList("role");
+        return new User(users.getUsername(), users.getPassword(), auths);
+        //return new User(s, pwd, AuthorityUtils.commaSeparatedStringToAuthorityList("admin,"));
     }
 }
