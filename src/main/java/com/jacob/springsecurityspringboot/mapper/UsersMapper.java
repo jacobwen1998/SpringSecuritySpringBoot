980c51ea49dcb1d6b00952eed441703047dd4ca7
xuezhou1998 <xuezhou1998@gmail.com>
2026-07-02 18:38:53 +0800

    UsersMapper

--- src/main/java/com/jacob/springsecurityspringboot/mapper/UsersMapper.java
+++ src/main/java/com/jacob/springsecurityspringboot/mapper/UsersMapper.java
@@ -1,7 +1,7 @@
 package com.jacob.springsecurityspringboot.mapper;
-import com.jacob.springsecurityspringboot.entity.*;
-
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+
 import com.jacob.springsecurityspringboot.entity.Users;
 
 public interface UsersMapper extends BaseMapper<Users> {
